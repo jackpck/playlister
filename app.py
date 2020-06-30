a915d1d08d0aef27811e7337bf3f2de8bbcb461f
jackypck <jackypck@bu.edu>
2020-06-30 16:41:08 -0400

    users can create playlists

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+'''
+create data/db in /System/Volumes/Data
+Run mongo daemon: sudo mongod --dbpath /System/Volumes/Data/data/db
+'''
+
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        # We know that embedded YouTube videos always have this format
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
 
 client = MongoClient()
 db = client.Playlister
@@ -13,6 +25,25 @@ def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
 
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos':videos,
+        'video_ids':video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
