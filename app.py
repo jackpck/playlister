fa40b9154a6bb7d9911f651022aa9c4abbdd9d26
jackypck <jackypck@bu.edu>
2020-06-30 14:04:08 -0400

    add mongodb and playlist model

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
-
-
-# OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 
 if __name__ == '__main__':
