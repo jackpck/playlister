d91bbb0cb0920cd86d5e8541301a965563b0e462
jackypck <jackypck@bu.edu>
2020-07-02 16:24:06 -0400

    fix host

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def video_url_creator(id_lst):
     return videos
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
-client = MongoClient(host=host)
+client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
