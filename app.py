2321bbc60ca0ccef24a89e3ed211e23b6456e0d5
jackypck <jackypck@bu.edu>
2020-07-02 12:32:54 -0400

    user can edit and destroy

--- app.py
+++ app.py
@@ -33,9 +33,22 @@ def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html')
 
+@app.route('/playlists/<playlist_id>/edit')
+def playlist_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html',playlist=playlist)
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlist_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id':ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
-    """Submit a new playlist."""
+    """Submit a new playlist. A placeholder of sort. Only deal with db.
+     Is never displayed. """
 
     video_ids = request.form.get('video_ids').split()
     videos = video_url_creator(video_ids)
@@ -53,6 +66,26 @@ def playlist_show(playlist_id):
     """Show a single playlist"""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html',playlist=playlist)
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlist_update(playlist_id):
+    """Submit an edited playlist. """
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    # create our updated playlist
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    # set the former playlist to the new one we just updated/edited
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    # take us back to the playlist's show page
+    return redirect(url_for('playlist_show', playlist_id=playlist_id))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
